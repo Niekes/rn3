4ca7b31f14573710d0866a70d8c2da1b715e5fb9
Stefan Nieke <info@niekes.com>
2021-05-23 14:05:29 +0200

    Add more canvas tests

--- tests/utils/canvas-test.js
+++ tests/utils/canvas-test.js
@@ -1,6 +1,6 @@
 import { test } from 'tape';
 import { select } from 'd3';
-import { createCanvas } from '../../src/utils/canvas';
+import { createCanvas, updateCanvas } from '../../src/utils/canvas';
 
 test('Create and append canvas with correct attributes', (t) => {
     select('body').html(null);
@@ -14,13 +14,38 @@ test('Create and append canvas with correct attributes', (t) => {
 
     document.body.appendChild(el);
 
-    const canvas = createCanvas('#el', '1234', 500, 750);
+    const canvas = createCanvas('#el', 'abc', 500, 750);
 
     t.equals(el.children.length, 1);
     t.equals(el.children[0].tagName, 'CANVAS');
-    t.equals(canvas.attr('id'), '1234');
+    t.equals(canvas.attr('id'), 'abc');
     t.equals(+canvas.attr('height'), 1000);
     t.equals(+canvas.attr('width'), 1500);
+
+    t.end();
+});
+
+test.only('Update canvas correctly', (t) => {
+    select('body').html(null);
+
+    const el = document.createElement('div');
+
+    el.setAttribute('id', 'el');
+
+    el.style.height = '500px';
+    el.style.width = '750px';
+
+    document.body.appendChild(el);
+
+    createCanvas('#el', 'abc', 500, 750);
+
+    const canvasUpdate = updateCanvas('#el', 'abc', 1000, 1500);
+
+    t.equals(el.children.length, 1);
+    t.equals(el.children[0].tagName, 'CANVAS');
+    t.equals(canvasUpdate.attr('id'), 'abc');
+    t.equals(+canvasUpdate.attr('height'), 2000);
+    t.equals(+canvasUpdate.attr('width'), 3000);
 
     t.end();
 });
