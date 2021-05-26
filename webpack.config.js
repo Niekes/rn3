ab3c911ff1899f54379e4247ce5f8322fdb56e1f
Stefan Nieke <info@niekes.com>
2021-05-26 16:08:46 +0200

    Add scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = (env, options) => ({
-    entry: './index.js',
+    entry: [
+        './index.scss',
+        './index.js',
+    ],
     output: {
         path: path.resolve(__dirname, 'src'),
         filename: 'rn3.min.js',
@@ -27,14 +31,40 @@ module.exports = (env, options) => ({
                     loader: 'babel-loader',
                 },
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'rn3.min.css',
+                        },
+                    },
+                    {
+                        loader: 'extract-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
         ],
     },
     optimization: {
         minimize: true,
-        minimizer: [new TerserPlugin({
-            include: /\.min\.js$/,
-            parallel: true,
-            extractComments: false,
-        })],
+        minimizer: [
+            new TerserPlugin({
+                include: /\.min\.js$/,
+                parallel: true,
+                extractComments: false,
+            }),
+            new CssMinimizerPlugin(),
+        ],
     },
 });
