0656e27ca6a3194de879af543aaebc7bc5be7680
Stefan Nieke <info@niekes.com>
2021-09-16 12:17:11 +0200

    ...

--- src/elements/datepicker/index.js
+++ src/elements/datepicker/index.js
@@ -1,3 +1,12 @@
+import {
+    select,
+    timeFormat,
+    timeMonday,
+    timeDay,
+    range,
+    timeMonth,
+} from 'd3';
+
 import Element from '../Element';
 
 import {
@@ -5,11 +14,13 @@ import {
 } from '../../utils/selection';
 
 import {
-    timeMonday,
-    timeDay,
     normalizeHours,
     getFirstOfMonth,
     getLastOfMonth,
+    isValidDate,
+    getDaysInMonth,
+    getISOWeeks,
+    getDateByWeek,
 } from '../../utils/date';
 
 import defaultSettings from './default-settings';
@@ -17,7 +28,11 @@ import defaultSettings from './default-settings';
 export default class Datepicker extends Element {
     #elements;
 
+    #periods;
+
     #today;
+
+    #counter;
 
     constructor(data) {
         super(data, defaultSettings);
@@ -31,7 +46,8 @@ export default class Datepicker extends Element {
         const display = appendSelection(form, 'div', { class: 'rn3-datepicker__form-display' });
         const arrow = appendSelection(form, 'div', { class: 'rn3-datepicker__form-arrow' });
         const periodControl = appendSelection(dropdown, 'div', { class: 'rn3-datepicker__dropdown-period-control' });
-        const customPeriodsWrapper = appendSelection(dropdown, 'div', { class: 'rn3-datepicker__dropdown-periods-wrapper' });
+        const periodsWrapper = appendSelection(dropdown, 'div', { class: 'rn3-datepicker__dropdown-periods-wrapper' });
+        const customPeriodsWrapper = appendSelection(dropdown, 'div', { class: 'rn3-datepicker__dropdown-custom-periods-wrapper' });
 
         this.#elements = {
             dropdown,
@@ -40,14 +56,47 @@ export default class Datepicker extends Element {
             display,
             arrow,
             periodControl,
+            periodsWrapper,
             customPeriodsWrapper,
+        };
+
+        this.#elements.icon.html(this.settings.form.icon);
+        this.#elements.arrow.html(this.settings.form.arrow);
+
+        /*
+            Init periods
+        */
+        this.#periods = {
+            selected: null,
+            day: {
+                marked: {
+                    from: null,
+                    to: null,
+                },
+                view: null,
+                format: timeFormat('%e'),
+            },
+            month: {
+                marked: {
+                    from: null,
+                    to: null,
+                },
+                view: null,
+                format: timeFormat('%b'),
+            },
+            week: {
+                marked: {
+                    from: null,
+                    to: null,
+                },
+                view: null,
+                format: timeFormat('%V'),
+            },
         };
 
         this.#today = new Date();
 
-        this.#elements.icon.html(this.settings.form.icon);
-        this.#elements.arrow.html(this.settings.form.arrow);
-        this.#elements.dropdown.html('HELLLOOO');
+        this.#counter = 0;
 
         /*
             Add event listener
@@ -93,28 +142,19 @@ export default class Datepicker extends Element {
         });
 
         /*
-            Check if custom periods were set
+            Set css classes
         */
-        if (this.settings.customPeriods.length === 0) {
-            this.#elements.customPeriodsWrapper.remove();
-        }
+        select(`#${this.id}`).classed('rn3-datepicker--day-mode', this.settings.activeMode === 'day');
+        select(`#${this.id}`).classed('rn3-datepicker--week-mode', this.settings.activeMode === 'week');
+        select(`#${this.id}`).classed('rn3-datepicker--month-mode', this.settings.activeMode === 'month');
 
         /*
             Convert dates
         */
         const convertedDates = this.#convertDates(this.data.values);
 
-        console.log(convertedDates);
-
-        /*
-            Set css classes
-        */
-        // this.setCssClassToWrapper('u3-date-picker--day-mode', this.settings.activeMode === 'day');
-        // this.setCssClassToWrapper('u3-date-picker--week-mode', this.settings.activeMode === 'week');
-        // this.setCssClassToWrapper('u3-date-picker--month-mode', this.settings.activeMode === 'month');
-
-        const from = this.#renderDate(this.data.values.from);
-        const to = this.#renderDate(this.data.values.to);
+        const from = this.#renderDate(convertedDates.from);
+        const to = this.#renderDate(convertedDates.to);
 
         /*
             Update date display
@@ -126,6 +166,22 @@ export default class Datepicker extends Element {
         if (from !== to) {
             this.#elements.display.text(`${from} - ${to}`);
         }
+
+        /*
+            Set view of date selection
+        */
+        this.#setView(convertedDates.from);
+
+        /*
+            Set the dates that should be marked and highlighted in the dropdown
+        */
+        this.#setDateMarkFrom(convertedDates.from);
+        this.#setDateMarkTo(convertedDates.to);
+
+        /*
+            Update date selection in dropdown
+        */
+        this.#updateDateSelection();
     }
 
     #renderDate = date => this.settings.modes[this.settings.activeMode].render(date)
@@ -141,17 +197,22 @@ export default class Datepicker extends Element {
     #toggleDropdown = (open) => {
         this.#elements.dropdown
             .classed('rn3-datepicker__dropdown--open', open);
-
-        if (!open) {
-            this.#elements.dropdown.html(null);
-        }
     }
+
+    #setView = (v) => {
+        this.#periods[this.settings.activeMode].view = v;
+    };
+
+    #setDateMarkFrom = (f) => {
+        this.#periods[this.settings.activeMode].marked.from = f;
+    };
+
+    #setDateMarkTo = (t) => {
+        this.#periods[this.settings.activeMode].marked.to = t;
+    };
 
     #convertDates = (dates) => {
         switch (this.settings.activeMode) {
-        case 'day': {
-            return Object.assign({}, dates);
-        }
         case 'month': {
             return {
                 from: getFirstOfMonth(dates.from),
@@ -164,8 +225,316 @@ export default class Datepicker extends Element {
                 to: timeDay.offset(timeMonday(dates.to), 6),
             };
         }
+        default: // day
+            return Object.assign({}, dates);
+        }
+    };
+
+    #getActiveMode = () => this.#periods[this.settings.activeMode];
+
+    #getDayData = (activeMode) => {
+        let dayData = null;
+
+        const dayControlData = [
+            {
+                text: '&lsaquo;',
+                value: -1,
+            },
+            {
+                text: timeFormat('%b \'%y')(activeMode.view),
+                value: null,
+            },
+            {
+                text: '&rsaquo;',
+                value: 1,
+            },
+        ];
+
+        const daysInMonth = getDaysInMonth(activeMode.view);
+        const firstOfMonth = getFirstOfMonth(activeMode.view).getDay();
+        const daysOffset = firstOfMonth === 0 ? 6 : firstOfMonth - 1;
+
+        dayData = range(1, daysInMonth + 1);
+
+        for (let j = 0; j < daysOffset; j += 1) {
+            dayData.unshift('&nbsp;');
+        }
+
+        const mon = timeMonday(this.#today);
+        const tue = timeDay.offset(mon, 1);
+        const wed = timeDay.offset(mon, 2);
+        const thu = timeDay.offset(mon, 3);
+        const fri = timeDay.offset(mon, 4);
+        const sat = timeDay.offset(mon, 5);
+        const sun = timeDay.offset(mon, 6);
+
+        dayData.unshift(
+            timeFormat('%a')(mon),
+            timeFormat('%a')(tue),
+            timeFormat('%a')(wed),
+            timeFormat('%a')(thu),
+            timeFormat('%a')(fri),
+            timeFormat('%a')(sat),
+            timeFormat('%a')(sun),
+        );
+
+        console.log(dayData, activeMode, this.settings.pages);
+
+        return {
+            control: dayControlData,
+            values: dayData
+                .map(d => ({
+                    value: typeof d === 'number' ? new Date(activeMode.view.getFullYear(), activeMode.view.getMonth(), d) : d,
+                })),
+        };
+    };
+
+    #getMonthData = (activeMode) => {
+        let monthData = null;
+
+        const monthControlData = [
+            {
+                text: '&lsaquo;',
+                value: -12,
+            },
+            {
+                text: timeFormat('%Y')(activeMode.view),
+                value: null,
+            },
+            {
+                text: '&rsaquo;',
+                value: 12,
+            },
+        ];
+
+        monthData = range(0, 12)
+            .map(m => ({
+                value: new Date(activeMode.view.getFullYear(), m, 1),
+            }));
+
+        return {
+            control: monthControlData,
+            values: monthData,
+        };
+    };
+
+    #getWeekData = (activeMode) => {
+        let weekData = null;
+
+        const weekControlData = [
+            {
+                text: '&lsaquo;',
+                value: -12,
+            },
+            {
+                text: timeFormat('%Y')(activeMode.view),
+                value: null,
+            },
+            {
+                text: '&rsaquo;',
+                value: 12,
+            },
+        ];
+
+        weekData = range(0, getISOWeeks(activeMode.view))
+            .map(m => ({
+                value: timeMonday(getDateByWeek(activeMode.view.getFullYear(), m + 1)),
+            }));
+
+        return {
+            control: weekControlData,
+            values: weekData,
+        };
+    };
+
+    #getData = () => {
+        const am = this.#getActiveMode();
+
+        switch (this.settings.activeMode) {
+        case 'month': {
+            return this.#getMonthData(am);
+        }
+        case 'week': {
+            return this.#getWeekData(am);
+        }
+        default: // day
+            return this.#getDayData(am);
+        }
+    };
+
+    #checkPeriod = (d, marked) => {
+        switch (this.settings.activeMode) {
+        case 'day': {
+            return {
+                isStart: d.getTime() === marked.from.getTime(),
+                isMiddle: d.getTime()
+                    >= marked.from.getTime()
+                    && d.getTime()
+                    <= marked.to.getTime(),
+                isEnd: d.getTime() === marked.to.getTime(),
+                isNow: d.getTime() === this.#today.getTime(),
+                isOutOfRange: d.getTime() < this.settings.minDate.getTime()
+                    || d.getTime() > this.settings.maxDate.getTime(),
+            };
+        }
+        case 'month': {
+            const sameYear = d.getFullYear() === marked.from.getFullYear();
+            return {
+                isStart: d.getMonth() === marked.from.getMonth() && sameYear,
+                isMiddle: d.getMonth() > marked.from.getMonth()
+                    && d.getMonth() < marked.to.getMonth() && sameYear,
+                isEnd: d.getMonth() === marked.to.getMonth() && sameYear,
+                isNow: d.getMonth() === this.#today.getMonth()
+                    && d.getFullYear() === this.#today.getFullYear(),
+            };
+        }
+        case 'week': {
+            const sameYear = d.getFullYear() === marked.from.getFullYear();
+            const sameWeek = timeFormat('%V')(d) === timeFormat('%V')(marked.from) && timeFormat('%V')(d) === timeFormat('%V')(marked.to);
+
+            if (timeFormat('%V')(d) === timeFormat('%V')(marked.to)) {
+                // TODO
+                // eslint-disable-next-line
+                console.log(timeFormat('%V')(d), timeFormat('%V')(marked.from), d);
+            }
+
+            return {
+                isStart: timeFormat('%V')(d) === timeFormat('%V')(marked.from) && sameYear && sameWeek,
+                isMiddle: timeFormat('%V')(d) > timeFormat('%V')(marked.from)
+                    && timeFormat('%V')(d) < timeFormat('%V')(marked.to)
+                    && sameYear && sameWeek,
+                isEnd: timeFormat('%V')(d) === timeFormat('%V')(marked.to) && sameYear && sameWeek,
+                isNow: timeFormat('%V')(d) === timeFormat('%V')(this.#today),
+            };
+        }
         default:
             throw new Error(`Selected mode does not exist: ${this.settings.activeMode}`);
         }
+    };
+
+    #updateDateSelection = () => {
+        /*
+            Get acive mode
+        */
+        const activeMode = this.#getActiveMode();
+
+        /*
+            Get the dates that will be marked and highlighted in the dropdown
+        */
+        const { marked } = activeMode;
+
+        /*
+            Get period data
+        */
+        const periodData = this.#getData();
+
+        /*
+            Create/update period control
+        */
+        const controlItems = this.#elements.periodControl
+            .selectAll('div.period-control-items')
+            .data(periodData.control);
+
+        controlItems
+            .enter()
+            .append('div')
+            .attr('class', 'period-control-items')
+            .on('click', (e, d) => {
+                e.stopPropagation();
+
+                if (!d.value) return;
+
+                const selPeriod = this.#getActiveMode().view;
+
+                const m = getFirstOfMonth(selPeriod);
+
+                this.#setView(timeMonth.offset(m, d.value));
+
+                this.#updateDateSelection();
+            })
+            .merge(controlItems)
+            .classed('period-control-items--disabled', d => !d.value)
+            .html(d => d.text);
+
+        controlItems
+            .exit()
+            .remove();
+
+        /*
+            Create/update period items
+        */
+        const periodItems = this.#elements.periodsWrapper.selectAll('div.period-items').data(periodData.values);
+
+        periodItems
+            .enter()
+            .append('div')
+            .attr('class', 'period-items')
+            .on('click', (e, d) => {
+                if (select(e.target).classed('period-items--out-of-range')) return;
+
+                const isFrom = this.settings.singleSelect ? true : this.#counter % 2 === 0;
+
+                if (isFrom) {
+                    this.#setDateMarkFrom(d.value);
+                }
+
+                this.#setDateMarkTo(d.value);
+
+                const { from } = marked;
+                const { to } = marked;
+
+                if (from.getTime() > to.getTime()) {
+                    this.#setDateMarkFrom(to);
+                    this.#setDateMarkTo(from);
+                }
+
+                this.#counter += 1;
+
+                this.#updateDateSelection();
+            })
+            .merge(periodItems)
+            .html((d, i, nodes) => {
+                const node = select(nodes[i]);
+                const isValid = isValidDate(d.value);
+
+                /*
+                    Reset all css classes
+                */
+                node.classed('period-items--valid-date', false);
+                node.classed('period-items--not-valid-date', false);
+                node.classed('period-items--start', false);
+                node.classed('period-items--end', false);
+                node.classed('period-items--middle', false);
+                node.classed('period-items--now', false);
+
+                node.classed('period-items--valid-date', isValid);
+                node.classed('period-items--not-valid-date', !isValid);
+
+
+                if (isValid) {
+                    const period = this.#checkPeriod(d.value, marked);
+
+                    node.classed('period-items--start', period.isStart);
+                    node.classed('period-items--end', period.isEnd);
+                    node.classed('period-items--middle', period.isMiddle);
+                    node.classed('period-items--now', period.isNow);
+                    node.classed('period-items--out-of-range', period.isOutOfRange);
+
+                    if (period.isMiddle && period.isStart) {
+                        node.classed('period-items--middle', false);
+                    }
+
+                    if (period.isMiddle && period.isEnd) {
+                        node.classed('period-items--middle', false);
+                    }
+
+                    return this.#periods[this.settings.activeMode].format(d.value).trim();
+                }
+                return d.value;
+            });
+
+        periodItems
+            .exit()
+            .remove();
     };
 }
