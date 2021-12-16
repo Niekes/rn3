623a0798eb11a73558e2a916774d54b347e92ed6
Stefan Nieke <info@niekes.com>
2021-12-16 18:38:09 +0100

    Add test for container

--- tests/utils/selection-test.mjs
+++ tests/utils/selection-test.mjs
@@ -1,6 +1,10 @@
 import { test } from 'tape';
 import { select } from 'd3';
-import { setMultiAttributes, setMultiStyles } from '../../src/utils/selection.js';
+import {
+    setMultiAttributes,
+    setMultiStyles,
+    createContainer,
+} from '../../src/utils/selection.js';
 
 test('Set multi attributes', (t) => {
     select('body').html(null);
@@ -54,6 +58,28 @@ test('Set multi styles', (t) => {
 
     t.equals(selection.style('background'), 'green');
     t.equals(selection.style('z-index'), '1000');
+
+    t.end();
+});
+
+test('Create container', (t) => {
+    select('body').html(null);
+
+    const el = document.createElement('div');
+
+    el.setAttribute('id', 'el');
+
+    el.style.height = '500px';
+    el.style.width = '750px';
+
+    document.body.appendChild(el);
+
+    const container = createContainer('#el', 'test', 'test-class');
+
+    t.equals(container.attr('id'), 'test');
+    t.equals(container.attr('class'), 'test-class');
+    t.equals(container.style('width'), '100%');
+    t.equals(container.style('height'), '100%');
 
     t.end();
 });
