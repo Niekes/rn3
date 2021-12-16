0a5ecc469957291f7b0c028f73f2a87059d5dfee
Stefan Nieke <info@niekes.com>
2021-12-16 18:22:11 +0100

    Add test for update pattern

---
+++ tests/utils/update-pattern-test.mjs
@@ -0,0 +1,41 @@
+import { test } from 'tape';
+import { select, easeCubic } from 'd3';
+import { updateSelection } from '../../src/utils/update-pattern.js';
+
+test.only('updateSelection', (t) => {
+    select('body').html(null);
+
+    const datum = {
+        join: {
+            cssClass: 'bars',
+            data: [{ id: 'a', value: 0 }],
+            identity: (d) => d.id,
+            parent: select('body'),
+        },
+        enter: {
+            x: 1,
+            y: 0,
+        },
+        update: {
+            x: 1,
+            y: 0,
+        },
+        exit: {
+            x: 1,
+            y: 0,
+        },
+    };
+
+    const transition = {
+        delay: null,
+        duration: 450,
+        ease: easeCubic,
+    };
+
+    const selection = updateSelection(datum, transition);
+
+    t.equals(selection.enter.empty(), false);
+    t.equals(selection.exit.empty(), true);
+
+    t.end();
+});
